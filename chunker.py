860a1538d9f1dffaa967aee537554c2599cf610f
Illic <miqueldreamer@gmail.com>
2018-06-01 23:28:55 +0000

    added chunker

---
+++ chunker.py
@@ -0,0 +1,15 @@
+def split(unchunked_text, limit):
+    chunks = []
+
+    while unchunked_text:
+        if len(unchunked_text) <= limit:
+            chunks.append(unchunked_text)
+            break
+        else:
+            index = unchunked_text.rfind("\n", 0, limit)
+            if index == -1:
+                return "can't do it"
+                break
+            chunks.append(unchunked_text[:index+1])
+            unchunked_text = unchunked_text[index+1:]
+    return chunks
