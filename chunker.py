37c041cd63597c153662af8d628c17123ac60cac
IllicLanthresh <miqueldreamer@gmail.com>
2018-06-05 22:44:04 +0200

    Solved issue with spaces at beginning of msg on discord

--- chunker.py
+++ chunker.py
@@ -6,9 +6,19 @@ def make_chunks(unchunked_text, limit):
             chunks.append(unchunked_text)
             break
         else:
+            unchunked_text = solve_spacing_at_beggining(unchunked_text)
+
             index = unchunked_text.rfind("\n", 0, limit)
+
             if index == -1:
                 return "can't do it"
+ 
             chunks.append(unchunked_text[:index+1])
             unchunked_text = unchunked_text[index+1:]
     return chunks
+
+def solve_spacing_at_beggining(text):
+    if(text.startswith("\t")):
+        text = text.replace("\t", "_ _   ", 1)
+    return text
+
