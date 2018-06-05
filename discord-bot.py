91eba29edd1af27dfa13ccf1886888c36aef08e7
IllicLanthresh <miqueldreamer@gmail.com>
2018-06-05 23:34:24 +0200

    modified commands' names and added !help command

--- discord-bot.py
+++ discord-bot.py
@@ -11,13 +11,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.startswith('!help'):
+        msg = 'list of commands:\n\t!hello\n\t!hammerhead\n\t!all ships\n\t!flight ready'
+        await client.send_message(message.channel, msg)
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
     if message.content.startswith('!hammerhead'):
         msg = 'rat1on thinks the hammerhead it\'s beautiful'
         await client.send_message(message.channel, msg)
-    if message.content.startswith('!all_ships'):
+    if message.content.startswith('!all ships'):
         msg = ""
 
         for ship in matrix.getAll():
@@ -25,7 +28,7 @@ async def on_message(message):
 
         for chunk in chunker.make_chunks(msg, 2000):
             await client.send_message(message.channel, chunk)
-    if message.content.startswith('!flight_ready'):
+    if message.content.startswith('!flight ready'):
         msg = "Flight Ready ships list:\n\n\n"
 
         for ship in matrix.getFlightReady():
